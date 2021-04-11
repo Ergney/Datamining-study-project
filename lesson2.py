986602645e52a5f417a65df7848812460a9f5729
Ergney <infimiya@yandex.ru>
2021-04-12 01:18:30 +0500

    Spat'

--- lesson2.py
+++ lesson2.py
@@ -1,10 +1,22 @@
 import requests
 from pathlib import Path
+import bs4
 
 url = 'https://magnit.ru/promo/'
 
 response = requests.get(url)
 
-file_path = Path(__file__).parent.joinpath('magnit.html')
+soup = bs4.BeautifulSoup(response.text, 'lxml')
 
-file_path.write_bytes(response.content)
+#auf = soup.find('div',attrs={'class':'сatalogue__main'})
+#z = auf.parent
+#t = auf.children
+#t1 = list(auf.children)
+#q = auf.find_all('a')
+#q1 = auf.find_all('a', recursive=False)
+#q1.source
+#q1[0].text
+#q1[0].find('div', attrs={'class': 'card-sale__title'})
+#q1[0].find('div', attrs={'class': 'card-sale__title'}).text
+
+#s = 'asfafs'
