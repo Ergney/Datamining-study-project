d5dd285856035263a94662ac2b79f79377a66a1e
Ergney <infimiya@yandex.ru>
2021-04-12 17:59:42 +0500

    WWW

--- lesson2.py
+++ lesson2.py
@@ -2,13 +2,18 @@ import requests
 from pathlib import Path
 import bs4
 
-url = 'https://magnit.ru/promo/'
+url = 'https://magnit.ru/promo/?geo=moskva'
 
 response = requests.get(url)
 
 soup = bs4.BeautifulSoup(response.text, 'lxml')
 
-#auf = soup.find('div',attrs={'class':'сatalogue__main'})
+html_promo = soup.find('div',attrs={'class':'сatalogue__main'})
+all_a_tag_promo = html_promo.find_all('a')
+all_a_tag_promo_dont_junk = html_promo.find_all('a', recursive=False)
+qwer = soup.find('a',attrs={'class':'card-sale'}).parent
+
+
 #z = auf.parent
 #t = auf.children
 #t1 = list(auf.children)
@@ -19,4 +24,5 @@ soup = bs4.BeautifulSoup(response.text, 'lxml')
 #q1[0].find('div', attrs={'class': 'card-sale__title'})
 #q1[0].find('div', attrs={'class': 'card-sale__title'}).text
 
-#s = 'asfafs'
+s = 'asfafs'
+
